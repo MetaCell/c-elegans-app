3a14bff0db906a76c11c4a2efdd34593a82186cb
aranega <vincent.aranega@gmail.com>
2024-03-25 11:14:46 -0600

    Add REST API script generation

---
+++ generate-binding.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Generates the openAPI specification
+OPENAPI_FOLDER="openapi/openapi.json"
+(cd backend && DJANGO_EXTRA_APPS="ninja" python manage.py export_openapi_schema --output "${OPENAPI_FOLDER}" --indent 2)
+
+
+# Generates the typescript API binding
+# (cd frontend && npm run generate-client)
