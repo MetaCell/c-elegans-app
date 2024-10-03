13c774fd1845e0a363161f6789ff1f57f387c006
aranega <vincent.aranega@gmail.com>
2024-10-03 05:37:27 -0600

    Add decorator for workspace and global context update

--- applications/visualizer/frontend/src/main.tsx
+++ applications/visualizer/frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import { enableMapSet } from "immer";
+import { enableMapSet, setAutoFreeze } from "immer";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { GlobalContextProvider } from "./contexts/GlobalContext.tsx";
 
 enableMapSet();
+setAutoFreeze(false);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <GlobalContextProvider>
