0689cd789a1534aef561a3e4a899f38b8f4b409e
Salam Dalloul <salamdalloul93@gmail.com>
2024-10-31 16:43:22 +0300

    fix linter issue

--- applications/visualizer/frontend/src/models/models.ts
+++ applications/visualizer/frontend/src/models/models.ts
@@ -37,7 +37,9 @@ export interface ThreeDViewerData {
 }
 
 function getRandomColor(): string {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, "0")}`;
 }
 export function getDefaultViewerData(visibility?: Visibility): ViewerData {
   return {
