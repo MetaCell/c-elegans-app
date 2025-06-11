6cdc878b44ebcab6ebe18fcf0b134af24f91eecc
aranega <vincent.aranega@gmail.com>
2025-06-11 09:21:45 -0600

    Add missing classes

---
+++ applications/visualizer/frontend/src/rest/models/GroupedConnection.ts
@@ -0,0 +1,10 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { ConnectionGroup } from './ConnectionGroup';
+export type GroupedConnection = {
+    neuron: string;
+    connections: Array<ConnectionGroup>;
+};
+
